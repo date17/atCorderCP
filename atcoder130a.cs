631a27a67650cbea439f9c42ccc4d1e0a58759e2
date17 <date17@ezweb.ne.jp>
2019-06-24 03:38:26 +0900

    130 15m 129 13m

---
+++ atcoder130a.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+// atcoder130a
+namespace atcoder130a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var value = Console.ReadLine();
+            var str = value.Split(' ');
+
+            var X = int.Parse(str[0]);
+            var A = int.Parse(str[1]);
+            if(X < A)
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+                Console.WriteLine("10");
+            }
+        }
+    }
+}

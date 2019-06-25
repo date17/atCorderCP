bbc0cc604552280c38287544a1212ae56b26c52e
date17 <date17@ezweb.ne.jp>
2019-06-25 14:49:32 +0900

    124a 5.25m

---
+++ atcoder124a.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+// atCoder124a
+
+namespace atcoder124a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var value = Console.ReadLine();
+            var lines = value.Split(' ');
+            var A = int.Parse(lines[0]);
+            var B = int.Parse(lines[1]);
+
+            var coin = 0;
+            for (int i = 0; i < 2; i++)
+            {
+                if (A < B)
+                {
+                    coin += B;
+                    B--;
+                }
+                else
+                {
+                    coin += A;
+                    A--;
+                }
+            }
+
+            Console.WriteLine(coin);
+        }
+
+    }
+}

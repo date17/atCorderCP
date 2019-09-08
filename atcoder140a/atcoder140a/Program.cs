058012e6c3cd14dbc9df5971016b03a82a4953cb
date17 <date17@ezweb.ne.jp>
2019-09-08 21:04:23 +0900

    140a 3min

---
+++ atcoder140a/atcoder140a/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+ // 3min
+namespace atcoder140a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = Console.ReadLine();
+
+            var N = int.Parse(n);
+
+            Console.WriteLine(N * N * N);
+
+            
+        }
+    }
+}

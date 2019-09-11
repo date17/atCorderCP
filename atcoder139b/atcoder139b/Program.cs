d3c6d490e1cef674c8f178f1c7222528436836b7
date17 <date17@ezweb.ne.jp>
2019-09-11 18:01:53 +0900

    139b 12min

---
+++ atcoder139b/atcoder139b/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+ // 12min
+
+namespace atcoder139b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var value= Console.ReadLine();
+            var sepaValue = value.Split(' ');
+            var A = int.Parse(sepaValue[0]);
+            var B = int.Parse(sepaValue[1]);
+
+            int ans = 0;
+            var count = 0;
+
+            if (A >= B)
+            {
+                ans = 1;
+                Console.WriteLine($"{ans}");
+            }
+            else
+            {
+                
+                count = B - A;
+
+                int newA = A - 1;
+                ans++;
+
+                while(count > 0)
+                {
+                    count -= newA;
+                    ans++;
+                }
+
+                Console.WriteLine($"{ans}");
+            }
+            
+        }
+    }
+}

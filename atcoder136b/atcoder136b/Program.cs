3f0f4346c17bb8910290d42d1c7070d8cf2ad2b4
date17 <date17@ezweb.ne.jp>
2019-09-24 17:42:54 +0900

    136b 25min

---
+++ atcoder136b/atcoder136b/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+//25min
+
+namespace atcoder136b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var getValue = Console.ReadLine();
+            var N = int.Parse(getValue);
+
+            int ketaCount = 0;
+
+            for (int i = 1; i <= N; i++)
+            {
+               if((0 < i && i < 10) || (100 <= i && i < 1000) || (10000 <= i && i < 100000))
+                {
+                    ketaCount++;
+                }
+            }
+
+            Console.WriteLine($"{ketaCount}");
+        }
+    }
+}
